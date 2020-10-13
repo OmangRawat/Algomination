a444a1aced761fc67cb3c17dd9c0cbe2dc33060c
rahultg2 <rsoni953804@gmail.com>
2020-10-14 00:12:15 +0530

    added some backend code

--- flowwithalgo/Algomination/views.py
+++ flowwithalgo/Algomination/views.py
@@ -6,4 +6,22 @@ def index(request):
     return render(request, 'Algomination/try4.html')
 
 def search(request):
-    return render(request, 'Algomination/search.html')
+#   return render(request, 'Algomination/search.html')
+    if request.method == "POST":
+        num = request.POST.get('num', '')
+        elements = request.POST.get('elements', '')
+        opt = request.POST.get('opt', '0')
+        ele = request.POST.get('ele', '')
+
+        elements = elements.split(" ")
+        flag = 0
+        for i in elements:
+            if i == ele:
+                index = elements.index(i)
+                flag = 1
+                break
+        if flag == 0:
+            index = -1
+
+        params = {'index' : index, 'elements' : elements, 'ele': ele, 'opt' : opt}
+    return render(request, 'Algomination/search.html', params)
