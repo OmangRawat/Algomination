0ecd4794fc92945d074786413d399e0611ae1626
rahultg2 <rsoni953804@gmail.com>
2020-10-21 01:07:08 +0530

    added some Backend code

--- flowwithalgo/Algomination/urls.py
+++ flowwithalgo/Algomination/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="Algomination"),
     path("search/", views.search, name="search"),
+    path("BubbleSort/", views.BubbleSort, name="BubbleSort"),
+    path("SelectionSort/", views.SelectionSort, name="SelectionSort"),
+    path("InsertionSort/", views.InsertionSort, name="InsertionSort"),
 ]
