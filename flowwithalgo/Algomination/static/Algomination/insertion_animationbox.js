0556c3c3ad7db6ec47c6fddffea6b1d26608709b
OmangRawat <56252621+OmangRawat@users.noreply.github.com>
2020-10-25 23:07:22 +0530

    Corrected the algorithm and animation of Insertion Sort

--- flowwithalgo/Algomination/static/Algomination/insertion_animationbox.js
+++ flowwithalgo/Algomination/static/Algomination/insertion_animationbox.js
@@ -23,6 +23,8 @@ function task1(i) {
         j = i - 1; 
         jelement = document.querySelector("#card" + (j + 1));
         counter = 0;
+        console.log(jelement);
+        //keyelement.style.backgroundColor = "red";
         while(j >= 0 && (parseFloat(jelement.innerText)) > key){   
             task(j);
             j = j-1;
@@ -33,57 +35,76 @@ function task1(i) {
             //console.log("j-1 element id: " )
         }  
         }console.log("inner loop end");
-        
+        //document.style.getElementById("#card0").id = ("#card" + (i + 1));
+        //keyelement = (document.getElementsByClassName("key"))[0];
+        //console.log((document.getElementsByClassName("key")));
         if (isNaN(keyelement.style.translateX))
             {
                 keyelement.style.translateX = 0;
             }
           
-        if(keyelement.innerText == 5){
-            console.log("outside loop before: " + keyelement.style.translateX + "i: " + i + " j: " + j);
-        }     
-        keyelement.style.translateX -= (166.5*counter);
-        if(keyelement.innerText == 5){
-            console.log("outside loop after: " + keyelement.style.translateX + "i: " + i + " j: " + j);
-        } 
+        // if(keyelement.innerText == 5){
+        //     console.log("outside loop before: " + keyelement.style.translateX + "i: " + i + " j: " + j);
+        // }     
+        console.log(keyelement);
+        //keyelement.style.translateX -= (166.5*counter);
+        keyelement.style.translateX -= (change*counter);
+        keyelement.id = "card0";
+        // if(keyelement.innerText == 5){
+        //     console.log("outside loop after: " + keyelement.style.translateX + "i: " + i + " j: " + j);
+        // } 
         anime
             ({
-                targets: "#" + keyelement.id,
+                //targets: "#" + keyelement.id,
+                targets: "#card0",
                 translateX: keyelement.style.translateX
             });
         //console.log("outside loop key element before change: " + keyelement.id);
         //console.log("tempelement outside: " + tempelement);
-        keyelement.id = tempelement;
+        //keyelement.id = tempelement;
+        keyelement.id = "card" + (i + 1 - counter);
+        //keyelement.style.backgroundColor = "blue";
+        //console.log(elems)
         //console.log("outside loop key element after change: " + keyelement.id);
-    }, 1000 * i * length); 
+        if(counter == 1)
+        {
+            document.getElementById('directions').innerText = "<--As [" + counter + "] element before the key element [" + key + "] is greater than it we insert " + key + " before it-->"; 
+        }
+        else
+        {
+            document.getElementById('directions').innerText = "<--As [" + counter + "] elements before the key element [" + key + "] are greater than it we insert " + key + " before them-->"; 
+        }
+    }, 500 * i * length); 
 }
 
 function task(j) { 
-    //console.log("j: "+ j);
+    console.log("j: "+ j);
     //setTimeout(function() { 
         counter++;
         if (isNaN(jelement.style.translateX))
             {
                 jelement.style.translateX = 0;
             }
-        if(jelement.innerText == 5){
-                console.log("inside loop before: " + keyelement.style.translateX + "i: " + i + " j: " + j);
-        } 
-        jelement.style.translateX += 166.5;
-        if(jelement.innerText == 5){
-            console.log("inside loop after: " + keyelement.style.translateX + "i: " + i + " j: " + j);
-        }
+        // if(jelement.innerText == 5){
+        //         console.log("inside loop before: " + keyelement.style.translateX + "i: " + i + " j: " + j);
+        // } 
+        //jelement.style.translateX += 166.5;
+        jelement.style.translateX += change;
+        // if(jelement.innerText == 5){
+        //     console.log("inside loop after: " + keyelement.style.translateX + "i: " + i + " j: " + j);
+        // }
         //console.log("translate of: " +  jelement.style.translateX + "element: " + jelement.innerText);
         anime
         ({
             targets: "#" + jelement.id,
             translateX: jelement.style.translateX
         });
-        //console.log("inside loop before change: " + jelement.id);
+        console.log("inside loop before change: " + jelement.id);
         if(counter == 1){
             jpluselement = document.querySelector("#card" + (j + 2));
             tempelement = jelement.id;
             jelement.id = jpluselement.id;
+            //jelement.classList.remove("key");
         }
         else{
             sectempelement = jelement.id;
@@ -93,7 +114,7 @@ function task(j) {
         }
         //console.log("inside loop after change: " + jelement.id);
         // j = j-1;
-        // console.log("j after: "+ j);
+        console.log("j after: "+ j);
         // if( (j-1) > 0){
         //     jelement = document.querySelector("#card" + (j+1));
         // }   
