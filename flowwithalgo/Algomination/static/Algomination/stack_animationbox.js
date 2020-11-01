32389ed21d45ad60d965f026dfdca72a5d4cbb02
OmangRawat <56252621+OmangRawat@users.noreply.github.com>
2020-11-01 16:38:36 +0530

    Added peek in Stack.js and changed some units in viewport mode

--- flowwithalgo/Algomination/static/Algomination/stack_animationbox.js
+++ flowwithalgo/Algomination/static/Algomination/stack_animationbox.js
@@ -20,26 +20,30 @@
 //     ele[index].id = "card" + (ele.length - index);
 //     //console.log(ele[index.id]);
 // }
+//console.log(window.screen.height)
+
+var unit = (77*(window.innerHeight)/100)
 
 function task1(sh)
 {
     setTimeout(function() {
         anime({
             targets: "#card0",
-            translateX: 408
+            translateX: 360             //408
         })
         task2(sh);
-    }, 2000);
+    }, 1000);
 }
 
 function task2(sh)
 {
+    //sh = (sh * unit)
     setTimeout(function() {
         anime({
             targets: "#card0",
             translateY: sh
         });
-    }, 2000); 
+    }, 1000); 
 }
 
 function task3(sh)
@@ -69,9 +73,10 @@ function task3(sh)
             targets: "#cardx",
 
             translateY: sh
+            //translate3D: (0, sh , 0)
         })
         task4();
-    }, 2000); 
+    }, 1000); 
 }
 
 function task4()
@@ -79,9 +84,9 @@ function task4()
     setTimeout(function() {
         anime({
             targets: "#cardx",
-            translateX: -408
+            translateX: -350                      //408
         });
-    }, 2000);
+    }, 1000);
 }
 
 // task1();
@@ -90,8 +95,27 @@ var operation = document.getElementById('stackani')
 console.log(operation)
 
 if(operation.querySelector('#pushani') != null){
-    
-    if (localStorage.getItem("card1") === null) {
+
+    if (localStorage.length == 10)
+    {
+        alert("Stack is Full");
+        var str = ""
+        var shift = 520 * (10 - localStorage.length)/10
+        console.log("shift1")
+        console.log(shift)
+        for (let index = localStorage.length; index > 0; index--) {
+            var local = localStorage.getItem('card' + index)
+            if(local == null)
+            {
+                break
+            }
+            str += "<span id ='cardtest' class = 'fill-blue card dbcard' style = 'transform: TranslateY("+ 0 +"px)'>" + "<p id='answer' style='margin: auto;'>"+ local +"</p>" + " </span>" 
+        }
+        document.getElementById('pushani').innerHTML = str
+        document.getElementById('directions').innerText = "<-- Stack is Full -->";
+        document.getElementById('storage_left').innerText = "0";
+    }
+    else if (localStorage.getItem("card1") === null) {
         // anime({
         //     targets: '#card0',
         //     translateY: 541
@@ -100,20 +124,30 @@ if(operation.querySelector('#pushani') != null){
         var value_card = document.getElementById('card0').innerText
         localStorage.setItem('card1', value_card)
         console.log(localStorage)
+        document.getElementById('storage_left').innerText = "" + (10 - localStorage.length);
+        var local = localStorage.getItem('card' + localStorage.length)
+        console.log(local)
+        document.getElementById('directions').innerText = "<-- Pushed [" + local + "] in the Stack -->";
     }
     else{
+        //console.log("............" + localStorage.length)
         var str = ""
         var shift = 520 * (10 - localStorage.length)/10
         console.log("shift1")
         console.log(shift)
         for (let index = localStorage.length; index > 0; index--) {
             var local = localStorage.getItem('card' + index)
-            
-            str += "<span id ='cardtest' class = 'fill-red card dbcard' style = 'transform: TranslateY("+ shift +"px)'>" + "<p id='answer' style='margin: auto;'>"+ local +"</p>" + " </span>" 
+            if(local == null)
+            {
+                break
+            }
+            str += "<span id ='cardtest' class = 'fill-blue card dbcard' style = 'transform: TranslateY("+ 0 +"vh)'>" + "<p id='answer' style='margin: auto;'>"+ local +"</p>" + " </span>" 
         }
 
         document.getElementById('pushani').innerHTML = str
-        shift = (520 - ((localStorage.length+1) * 52))
+        //shift = (520 - ((localStorage.length + 1) * 52))
+        //shift = ((70 - (7 * localStorage.length))*(unit/100))
+        shift = (75 - (7 * localStorage.length))*(unit/100)
         console.log("shift2")
         console.log(shift)
         // anime({
@@ -125,12 +159,18 @@ if(operation.querySelector('#pushani') != null){
         var key = 'card' + (localStorage.length + 1)
         localStorage.setItem(key, value_card)
         console.log(localStorage)
+        document.getElementById('storage_left').innerText = "" + (10 - localStorage.length);
+        var local = localStorage.getItem('card' + localStorage.length)
+        console.log(local)
+        document.getElementById('directions').innerText = "<-- Pushed [" + local + "] in the Stack -->";
     }   
 }
 else if(operation.querySelector('#popani') != null){
     
     if(localStorage.length == 0){
         alert("Stack is Empty!!!!");
+        document.getElementById('storage_left').innerText = "10";
+        document.getElementById('directions').innerText = '<-- Stack is Empty -->';
     }
 
     var str = ""
@@ -140,11 +180,13 @@ else if(operation.querySelector('#popani') != null){
     for (let index = localStorage.length; index > 0; index--) {
         var local = localStorage.getItem('card' + index)
         
-        str += "<span id ='cardtest' class = 'fill-red card dbcard' style = 'transform: TranslateY("+ shift +"px)'>" + "<p id='answer' style='margin: auto;'>"+ local +"</p>" + " </span>" 
+        // str += "<span id ='cardtest' class = 'fill-blue card dbcard' style = 'transform: TranslateY("+ ((529 * (10 - localStorage.length))/77) +"vh)'>" + "<p id='answer' style='margin: auto;'>"+ local +"</p>" + " </span>" 
+        str += "<span id ='cardtest' class = 'fill-blue card dbcard' style = 'transform: TranslateY("+ 0 +"px)'>" + "<p id='answer' style='margin: auto;'>"+ local +"</p>" + " </span>"
     }
 
     document.getElementById('popani').innerHTML = str
-    shift = (520 - ((localStorage.length+1) * 52))*(-1)
+    //shift = (520 - ((localStorage.length + 1) * 52))*(-1)
+    shift = (-1*(70 - (6.7 * localStorage.length))*(unit/100))
     console.log("shift2")
     console.log(shift)
     // anime({
@@ -152,6 +194,38 @@ else if(operation.querySelector('#popani') != null){
     //     translateY: shift
     // })
     task3(shift)
-    localStorage.removeItem('card'+localStorage.length)
+    if(localStorage.length != 0)
+    {
+        document.getElementById('storage_left').innerText = "" + (11 - localStorage.length);
+        var local = localStorage.getItem('card' + localStorage.length)
+        console.log(local)
+        document.getElementById('directions').innerText = "<-- Removed [" + local + "] from the Stack -->"
+        localStorage.removeItem('card'+localStorage.length)
+    }
 }
+else if(operation.querySelector('#peekani') != null)
+{
+    if(localStorage.length == 0){
+        alert("Stack is Empty!!!!");
+        document.getElementById('storage_left').innerText = "10";
+        document.getElementById('directions').innerText = '<-- Stack is Empty -->';
+    }
 
+    var str = ""
+    var shift = 520 * (10 - localStorage.length)/10
+    console.log("shift1")
+    console.log(shift)
+    for (let index = localStorage.length; index > 0; index--) {
+        var local = localStorage.getItem('card' + index)
+        
+        str += "<span id ='cardtest' class = 'fill-blue card dbcard' style = 'transform: TranslateY("+ 0 +"vh)'>" + "<p id='answer' style='margin: auto;'>"+ local +"</p>" + " </span>" 
+    }
+    if(localStorage.length != 0)
+    {
+        document.getElementById('peekani').innerHTML = str
+        var local = localStorage.getItem('card' + localStorage.length)
+        console.log(local)
+        document.getElementById('directions').innerText = '<-- Element at the top of the Stack is [' + local + '] -->';
+        document.getElementById('storage_left').innerText = 10 - localStorage.length;
+    }
+}
