bdc2217bb53b5ce6d217007077a54e06fff7accf
OmangRawat <omangrawat1990@gmail.com>
2023-09-03 02:31:21 +0530

    Added Static files

--- flowwithalgo/settings.py
+++ flowwithalgo/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Managing media
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
