b8c19dcca6ccb9bb354a3d8cd4b3715bf15cb407
OmangRawat <omangrawat1990@gmail.com>
2023-09-08 00:40:31 +0530

    For Railway

--- settings.py
+++ settings.py
@@ -23,8 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vx8m$wvc6$fzbqq7xvo92g)cgq^fb*_xfgy!gomu&o$g9kijk#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['algomination.herokuapp.com', '127.0.0.1']
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,13 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Managing media
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
