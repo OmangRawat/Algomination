bcc66babf38a2da72355730b05cd57a31d2b4f51
OmangRawat <omangrawat1990@gmail.com>
2023-09-08 01:07:32 +0530

    For Railway

--- flowwithalgo/settings.py
+++ flowwithalgo/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'vx8m$wvc6$fzbqq7xvo92g)cgq^fb*_xfgy!gomu&o$g9kijk#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['algomination.herokuapp.com', '127.0.0.1', 'algomination.up.railway.app']
+
+ALLOWED_HOSTS = ['algomination.up.railway.app']
 
 
 # Application definition
@@ -40,8 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
